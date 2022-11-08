da3c93c64f378a274f71c8d0fe0c0ab5ca8220fc
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-11-08 21:10:17 +0700

    Use dotenv to access information

--- app-detect-object.js
+++ app-detect-object.js
@@ -1,21 +1,23 @@
+require('dotenv').config();
 const util = require('util');
 const fs = require('fs');
 const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
 const msRest = require("@azure/ms-rest-js");
 
-const publishIterationName = "Iteration5";
-const projectId = "93395755-61d4-44ce-89b6-33ccb17c63b9";
 
-const predictionKey = "e64f898565724f03a83287b0121b96e6";
-const predictionResourceId = "/subscriptions/17a8d214-246e-4828-9bc4-d4304921e2c4/resourceGroups/dotnetconf/providers/Microsoft.CognitiveServices/accounts/webinar_custom_vision";
-const predictionEndpoint = "https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/93395755-61d4-44ce-89b6-33ccb17c63b9/detect/iterations/Iteration5/image";
+const publishIterationName = process.env.publishIterationName;
+const projectId = process.env.projectId;
+
+const predictionKey = process.env.predictionKey;
+const predictionResourceId = process.env.predictionResourceId;
+const predictionEndpoint = process.env.predictionEndpoint;
 
 const predictor_credentials = new msRest.ApiKeyCredentials({ inHeader: { "Prediction-key": predictionKey } });
 const predictor = new PredictionApi.PredictionAPIClient(predictor_credentials, predictionEndpoint);
 
 
 (async () => {
-    const testFile = fs.readFileSync(`Test/sushi/sushi_2.jpg`);
+    const testFile = fs.readFileSync(`Test/sushi/sushi_1.jpg`);
 
     const results = await predictor.detectImage(projectId, publishIterationName, testFile);
     
