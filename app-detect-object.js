f09b6f95ab5c7be5f50bd7a3deea6b7a30a40e97
teerasej <teerasej@gmail.com>
2022-11-08 11:07:52 +0700

    add detection api's information

--- app-detect-object.js
+++ app-detect-object.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
 const msRest = require("@azure/ms-rest-js");
 
-const publishIterationName = "Iteration1";
+const publishIterationName = "Iteration5";
 const projectId = "93395755-61d4-44ce-89b6-33ccb17c63b9";
 
 const predictionKey = "e64f898565724f03a83287b0121b96e6";
-const predictionResourceId = "/subscriptions/17a8d214-246e-4828-9bc4-d4304921e2c4/resourceGroups/app-discovery-day/providers/Microsoft.CognitiveServices/accounts/completecustomvision-Prediction";
-const predictionEndpoint = "https://completecustomvision-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/be46a6e0-bfa2-4aaa-a26d-895d29186083/classify/iterations/Iteration1/image";
+const predictionResourceId = "/subscriptions/17a8d214-246e-4828-9bc4-d4304921e2c4/resourceGroups/dotnetconf/providers/Microsoft.CognitiveServices/accounts/webinar_custom_vision";
+const predictionEndpoint = "https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/93395755-61d4-44ce-89b6-33ccb17c63b9/detect/iterations/Iteration5/image";
 
 const predictor_credentials = new msRest.ApiKeyCredentials({ inHeader: { "Prediction-key": predictionKey } });
 const predictor = new PredictionApi.PredictionAPIClient(predictor_credentials, predictionEndpoint);
